44ddd3ec5e7a6c48c9052bd87742eddc17cd5d1d
MD. Nafis Hussain <mnh.nafis14@gmail.com>
2025-06-22 02:52:15 +0600

    The parser files are added ???

--- perser/declarations.h
+++
@@ -1,8 +0,0 @@
-#include <string>
-
-// defined in C8086Parser.g4 file
-void writeIntoparserLogFile(const std::string message);
-void writeIntoErrorFile(const std::string message);
-
-
-// defined in functions.cpp
