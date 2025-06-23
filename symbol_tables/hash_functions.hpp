8a1baf5688131a9615b7d2eb015bedb46df184da
MD. Nafis Hussain <mnh.nafis14@gmail.com>
2025-06-23 21:57:24 +0600

    Split hash_functions.hpp into declaration and definition

--- symbol_tables/hash_functions.hpp
+++ symbol_tables/hash_functions.hpp
@@ -6,55 +6,18 @@
 #include "./tools.hpp"
 
 
-unsigned int sdbm_hash(std::string str, unsigned int num_buckets){
-    unsigned int hash = 0;
-    unsigned int len = str.length();
+unsigned int sdbm_hash(std::string str, unsigned int num_buckets);
 
-    for(unsigned int i=0; i<len; i++){
-        hash = (str[i]) + (hash << 6) + (hash << 16) - hash;
-    }
-    
-    return hash % num_buckets;
-}
+unsigned int sdbm_hash2(std::string str, unsigned int num_buckets);
 
-unsigned int sdbm_hash2(std::string str, unsigned int num_buckets){
-    unsigned int hash = 0;
-    unsigned int len = str.length();
-
-    for(unsigned int i=0; i<len; i++){
-        hash = ((str[i]) + (hash << 6) + (hash << 16) - hash) % num_buckets;
-    }
-    
-    return hash;
-}
-
-unsigned int sdbm_hash(std::string str){
-    return sdbm_hash(str, 53);
-}
+unsigned int sdbm_hash(std::string str);
 
 // SOURCE: http://www.cse.yorku.ca/~oz/hash.html
-unsigned int djb2_hash(std::string str, unsigned int bucketsize){
-    unsigned long int hash = 5381;
-    for(int i=0; i<str.length(); i++){
-        hash = (hash << 5) + hash + (unsigned char)str.at(i);
-    }
-    
-    return (unsigned int)hash % bucketsize;
-}
+unsigned int djb2_hash(std::string str, unsigned int bucketsize);
 
 // Reference: http://www.isthe.com/chongo/tech/comp/fnv/
 // And,     : https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
-unsigned int fnv1a_hash(std::string str, unsigned int bucketsize){
-    uint32_t hash = 2166136261u; // FNV offset basis for 32 bit;
-    for(int i=0; i<str.length(); i++){
-        hash = hash ^ (unsigned char)str.at(i);
-        hash = hash * 16777619u; //FNV prime for 32 bit
-    }
-    return (unsigned int)hash % bucketsize;
-}
-
-
-
+unsigned int fnv1a_hash(std::string str, unsigned int bucketsize);
 
 
 
